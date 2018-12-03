b20796b70e005d4f67f573ee34ea0d12bcf1c1db
Administrator <zhangzhenzhen1999@163.com>
2018-12-03 09:30:43 +0800

    百度地图API

--- src/main/java/com/example/administrator/baidumap/MainActivity.java
+++ src/main/java/com/example/administrator/baidumap/MainActivity.java
@@ -26,14 +26,15 @@ public class MainActivity extends AppCompatActivity {
     private MapView mMapView = null;
     private BaiduMap bdMap;
     private LocationClient mylocationClient; //定位服务
-    private MylocationListener mylistener;//重写监听类，获取我的位置
     private float myCurrentX;
     private Context context;
     private double myLatitude;//纬度，用于存储自己所在位置的纬度
     private double myLongitude;//经度，用于存储自己所在位置的经度
     private BitmapDescriptor myIconLocation1;
-    private MyOrientationListener myOrientationListener; //方向感应类对象
     private MyLocationConfiguration.LocationMode locationMode;  //定位图层显示方式
+    private boolean isFirstLocation = true;
+    private MyLocationListener myListener = new MyLocationListener();
+
 
 
 
@@ -45,12 +46,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.context=this;
         initView();//初始化窗口
-        initLocation(); //获取到当前用户的位置
         button=(Button)findViewById(R.id.locate);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getLocationByLL(myLatitude, myLongitude);
             }
         });
     }
@@ -58,24 +57,14 @@ public class MainActivity extends AppCompatActivity {
      * 此方法用于更新当前用户的位置信息
      * */
     private void initLocation() {
-        locationMode=MyLocationConfiguration.LocationMode.NORMAL;
-        mylocationClient = new LocationClient(this);
-        mylistener = new MylocationListener();
-        mylocationClient.registerLocationListener(mylistener);
+        locationMode = MyLocationConfiguration.LocationMode.NORMAL;
+
         LocationClientOption mOption = new LocationClientOption(); //配置当前位置参数
         mOption.setCoorType("bd0911"); //设置坐标类型
         mOption.setIsNeedAddress(true);
         mOption.setOpenGps(true);//设置是否打开GPS
         mOption.setScanSpan(1000);//设置扫描时间间隔1000ms
         mylocationClient.setLocOption(mOption);
-        myOrientationListener = new MyOrientationListener(context);
-
-        myOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
-            @Override
-            public void onOrientationChanged(float x) {
-
-            }
-        });
     }
 
     /*
@@ -86,43 +75,30 @@ public class MainActivity extends AppCompatActivity {
         bdMap = mMapView.getMap();//根据给定增量显示地图大小
         MapStatusUpdate msu= MapStatusUpdateFactory.zoomTo(18.0f);
         bdMap.setMapStatus(msu);
+        bdMap.setMyLocationEnabled(true);
+        mylocationClient=new LocationClient(this);
+        mylocationClient.registerLocationListener(myListener);
+        initLocation();
+        mylocationClient.start();
     }
+    private class MyLocationListener implements BDLocationListener {
 
-
-    private class MylocationListener implements BDLocationListener {
-
-        private boolean isFirstIn=true;
         @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            myLatitude = bdLocation.getLatitude();
-            myLongitude = bdLocation.getLongitude();
+        public void onReceiveLocation(BDLocation location) {
             MyLocationData data = new MyLocationData.Builder()
-                    .direction(myCurrentX)//设定图标方向
-                    .accuracy(bdLocation.getRadius())//getRadius 获取定位精度,默认值0.0f
-                    .latitude(myLatitude)//百度纬度坐标
-                    .longitude(myLongitude)//百度经度坐标
+                    .accuracy(location.getRadius())
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
                     .build();
-            //设置定位数据, 只有先允许定位图层后设置数据才会生效，参见 setMyLocationEnabled(boolean)
+
             bdMap.setMyLocationData(data);
-            //判断是否为第一次定位,是的话需要定位到用户当前位置
-            if (isFirstIn) {
-                //根据当前所在位置经纬度前往
-                getLocationByLL(myLatitude, myLongitude);
-                isFirstIn = false;
-                //提示当前所在地址信息
-//                Toast.makeText(context, bdLocation.getAddrStr(), Toast.LENGTH_SHORT).show();
+            if (isFirstLocation) {
+                LatLng latLng = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+                bdMap.animateMapStatus(msu); //动画效果
+                isFirstLocation = false;
             }
         }
-    }
-    /*
-    * 获取当前位置的经纬度
-    * */
-    public void getLocationByLL(double la, double lg)
-    {
-        //地理坐标的数据结构
-        LatLng latLng = new LatLng(la, lg);
-        //描述地图状态将要发生的变化,通过当前经纬度来使地图显示到该位置
-        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
-        bdMap.setMapStatus(msu);
     }
 }
